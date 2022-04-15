ca5a2ef4ede368c98302e0612acfed1d8f5b0dd7
AAVN\lchai <lchai@axonactive.com>
2022-04-15 16:59:16 +0700

    add animation for background

---
+++ script.js
@@ -0,0 +1,9 @@
+// @function generateStars($n) {
+//     $value: "#{0} #{random(2000)}px #{random(2000)}px #FFF";
+  
+//     @for $i from 2 through $n {
+//       $value: "#{$value} , #{random(2000)}px #{random(2000)}px #FFF";
+//     }
+  
+//     @return unquote($value);
+// }
