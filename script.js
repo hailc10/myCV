3dc203f6ec54efcb80e0e6aa2666bfcd0e95430a
AAVN\lchai <lchai@axonactive.com>
2022-04-18 15:09:34 +0700

    finish CV

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// @function generateStars($n) {
-//     $value: "#{0} #{random(2000)}px #{random(2000)}px #FFF";
-  
-//     @for $i from 2 through $n {
-//       $value: "#{$value} , #{random(2000)}px #{random(2000)}px #FFF";
-//     }
-  
-//     @return unquote($value);
-// }
+const ball = document.querySelector('.switch');
+const lightThemeBtn = document.querySelector('.wrapper');
+const lightThemeCanvas = document.getElementById('body');
+lightThemeBtn.addEventListener('click', function(){
+    if(lightThemeCanvas.classList.contains('light-theme')){
+        lightThemeCanvas.classList.remove('light-theme')
+    }
+    else{
+        lightThemeCanvas.classList.add('light-theme')
+    }
+});
