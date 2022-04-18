9939fc62b2cc1d68db5eedbc1ecc3198db089ae1
AAVN\lchai <lchai@axonactive.com>
2022-04-18 17:05:11 +0700

    add href to a card

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const lightThemeCanvas = document.getElementById('body');
 lightThemeBtn.addEventListener('click', function(){
     if(lightThemeCanvas.classList.contains('light-theme')){
         lightThemeCanvas.classList.remove('light-theme')
-    }
+    }   
     else{
         lightThemeCanvas.classList.add('light-theme')
     }
